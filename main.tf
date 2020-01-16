831834f24a9cee89e90e8a34273236d1f84fd25f
kumar44424 <45359851+kumar44424@users.noreply.github.com>
2020-01-16 11:05:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -415,7 +415,7 @@ resource "aws_network_interface" "acme_pafw_instance_private" {
 resource "aws_instance" "RHEL" {
   instance_type               = "t2.micro"
   ami                         = "ami-003b12a9a1ee83922"
-  subnet_id                   = "${aws_subnet.cam_aws_subnet_public.id}"
+  subnet_id                   = "${aws_subnet.cam_aws_subnet_private.id}"
   vpc_security_group_ids      = ["${aws_security_group.cam_aws_sg.id}"]
   key_name                    = "${aws_key_pair.temp_public_key.id}"
   associate_public_ip_address = true
